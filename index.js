b8ebeca7dfa415b78d4b002aa3bc32594c52b102
Will <willpeng97@gmail.com>
2023-12-07 17:01:40 +0800

    '123'

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3001;
+
+app.get("/", (req, res) => res.type('html').send(html));
+
+const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+server.keepAliveTimeout = 120 * 1000;
+server.headersTimeout = 120 * 1000;
+const html=
+`
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -22,3 +34,4 @@
     <!-- <script src="./js/girdData.js"></script> -->
   </body>
 </html>
+`
